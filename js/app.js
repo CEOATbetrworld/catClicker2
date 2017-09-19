9d4e9a180236145c2ccab7470694cb28ce8b1951
DHRUV KUMAR <dkiphones620@gmail.com>
2017-09-19 15:52:10 +0530

    knockout forEach

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-let ViewModel = function(){
- this.clickCount =  ko.observable(0);
- this.name = ko.observable('Tabby');
- this.imgSrc = ko.observable('img/cat1.jpg');
+let Cat = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/cat1.jpg');
+    this.nickNames = ko.observableArray(["hey", "hi", "hello"]);
 
- this.incrementCounter  = function(){
- 	this.clickCount(this.clickCount() + 1);
- 	if(this.clickCount() === 10){
-     this.name('Woo hoo level up :)');   
- 	}
- };
+}
+
+
+let ViewModel = function() {
+    this.currentCat = ko.observable(new Cat());
+    this.incrementCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+
+        if (this.currentCat().clickCount() === 5) {
+            this.currentCat().name("woo hoo upgrade");
+        }
+    };
 }
 
 ko.applyBindings(new ViewModel());
